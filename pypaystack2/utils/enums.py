42af6fc83ef8f0ed01cfe8f68458a681d5b6050a
Gbenga Adeyi <adeyigbenga005@gmail.com>
2023-07-27 10:52:00 +0100

    Refactor utils

---
+++ pypaystack2/utils/enums.py
@@ -0,0 +1,209 @@
+from enum import Enum
+from typing import Optional
+
+
+class HTTPMethod(str, Enum):
+    """An enum of supported http methods"""
+
+    GET = "GET"
+    POST = "POST"
+    PUT = "PUT"
+    PATCH = "PATCH"
+    DELETE = "DELETE"
+    OPTIONS = "OPTIONS"
+    HEAD = "HEAD"
+
+
+class TerminalEvent(str, Enum):
+    """Enum of the types of events supported by Terminal API"""
+
+    TRANSACTION = "transaction"
+    INVOICE = "invoice"
+
+
+class TerminalEventAction(str, Enum):
+    PROCESS = "process"
+    VIEW = "view"
+    PRINT = "print"
+
+
+class Currency(str, Enum):
+    """Enum of currencies supported by paystack."""
+
+    NGN = "NGN"
+    GHS = "GHS"
+    ZAR = "ZAR"
+    USD = "USD"
+    KES = "KES"
+    XOF = "XOF"
+    EGP = "EGP"
+
+
+class Interval(str, Enum):
+    """Enum of intervals supported by paystack."""
+
+    HOURLY = "hourly"
+    DAILY = "daily"
+    WEEKLY = "weekly"
+    MONTHLY = "monthly"
+    ANNUALLY = "annually"
+
+
+class Channel(str, Enum):
+    """Enum of payment channels supported by paystack"""
+
+    CARD = "card"
+    BANK = "bank"
+    USSD = "ussd"
+    QR = "qr"
+    MOBILE_MONEY = "mobile_money"
+    BANK_TRANSFER = "bank_transfer"
+
+
+class Bearer(str, Enum):
+    """Enum for who bears paystack charges"""
+
+    ACCOUNT = "account"
+    SUB_ACCOUNT = "subaccount"
+    ALL_PROPORTIONAL = "all-proportional"
+    ALL = "all"
+
+
+class TransactionStatus(str, Enum):
+    """Enum of transaction status"""
+
+    FAILED = "failed"
+    SUCCESS = "success"
+    ABANDONED = "abandoned"
+
+
+class Split(str, Enum):
+    """Enum of split types"""
+
+    PERCENTAGE = "percentage"
+    FLAT = "flat"
+
+
+class Country(str, Enum):
+    """Enum of countries supported by paystack"""
+
+    NIGERIA = "NG"
+    GHANA = "GH"
+    SOUTH_AFRICA = "ZA"
+    KENYA = "KE"
+    COTE_D_IVOIRE = "CI"
+    EGYPT = "EG"
+
+    @staticmethod
+    def get_full(value: str) -> Optional[str]:
+        """Returns paystack supported country name in full lowercase
+
+        Args:
+            value: The two-digit iso name of the country.
+
+        Returns:
+            The name of the country in lowercase if it is supported by
+            paystack or none.
+        """
+        value = value.lower()
+        return {
+            "ng": "nigeria",
+            "gh": "ghana",
+            "za": "south africa",
+            "ke": "kenya",
+            "ci": "côte d'ivoire",
+            "eg": "egypt",
+        }.get(value)
+
+
+class RiskAction(str, Enum):
+    """Enum of RiskActions supported by paystack"""
+
+    DEFAULT = "default"
+    WHITELIST = "allow"
+    BLACKLIST = "deny"
+
+
+class Identification(str, Enum):
+    """Enum of Identification methods supported by paystack"""
+
+    BVN = "bvn"
+    BANK_ACCOUNT = "bank_account"
+
+
+class RecipientType(str, Enum):
+    """Enum of Transfer Recipient types"""
+
+    NUBAN = "nuban"
+    MOBILE_MONEY = "mobile_money"
+    BASA = "basa"
+
+
+class Document(str, Enum):
+    """Enum of Document types supported by paystack"""
+
+    IDENTITY_NUMBER = "identityNumber"
+    PASSPORT_NUMBER = "passportNumber"
+    BUSINESS_REGISTRATION_NUMBER = "businessRegistrationNumber"
+
+
+class Status(str, Enum):
+    """Enum of statuses supported by paystack, used by Invoice, Charge & Plan"""
+
+    PENDING = "pending"
+    SUCCESS = "success"
+    FAILED = "failed"
+
+
+class Schedule(str, Enum):
+    """Enum of settlement schedules supported by paystack"""
+
+    AUTO = "auto"
+    WEEKLY = "weekly"
+    MONTHLY = "monthly"
+    MANUAL = "manual"
+
+
+class Reason(str, Enum):
+    """Enum of Reset OTP options"""
+
+    RESEND_OTP = "resend_otp"
+    TRANSFER = "transfer"
+    DISABLE_OTP = "disable_otp"
+
+
+class Gateway(str, Enum):
+    """Enum of bank gateways supported by paystack"""
+
+    EMANDATE = "emandate"
+    DIGITALBANKMANDATE = "digitalbankmandate"
+
+
+class AccountType(str, Enum):
+    """Enum of Account types supported by paystack"""
+
+    PERSONAL = "personal"
+    BUSINESS = "business"
+
+
+class Resolution(str, Enum):
+    """Enum of Resolutions supported by paystack"""
+
+    MERCHANT_ACCEPTED = "merchant-accepted"
+    DECLINED = "declined"
+
+
+class BankType(str, Enum):
+    """Enum of bank types"""
+
+    GHIPPS = "ghipps"
+    MOBILE_MONEY = "mobile_money"
+
+
+class DisputeStatus(str, Enum):
+    """Enum of dispute status supported by paystack"""
+
+    PENDING = "pending"
+    RESOLVED = "resolved"
+    AWAITING_BANK_FEEDBACK = "awaiting-bank-feedback"
+    AWAITING_MERCHANT_FEEDBACK = "awaiting-merchant-feedback"
---
+++ pypaystack2/utils/models.py
@@ -0,0 +1,330 @@
+from dataclasses import dataclass
+from typing import Any, Optional, Union, NamedTuple
+
+
+@dataclass
+class BulkChargeInstruction:
+    """A dataclass for bulk charge instruction.
+
+    Attributes:
+        authorization: The authorization code of the customer you want to charge.
+        amount: The amount you want to charge.
+        reference: The transaction reference.
+    """
+
+    authorization: str
+    amount: int
+    reference: str
+
+    @property
+    def dict(self) -> dict:
+        """Converts a BulkChargeInstruction to a dictionary"""
+        return {
+            "authorization": self.authorization,
+            "amount": self.amount,
+            "reference": self.reference,
+        }
+
+    @classmethod
+    def from_dict(cls, value: dict) -> "BulkChargeInstruction":
+        """Creates a BulkChargeInstruction from a dictionary.
+
+        The dictionary has to have the following keys: `authorization`, `amount`, `reference`
+        """
+        return cls(
+            authorization=value["authorization"],
+            amount=value["amount"],
+            reference=value["reference"],
+        )
+
+    @classmethod
+    def from_dict_many(cls, values: list[dict]) -> list["BulkChargeInstruction"]:
+        """Creates a list of BulkChargeInstruction from a list of dictionaries.
+
+        The dictionaries must have the following keys: `authorization`, `amount`, `reference`
+        """
+        return [
+            cls(
+                authorization=item["authorization"],
+                amount=item["amount"],
+                reference=item["reference"],
+            )
+            for item in values
+        ]
+
+
+@dataclass
+class LineItem:
+    """A dataclass for LineItem.
+
+    Attributes:
+        name: The name of the product.
+        amount: The price of the product.
+        quantity: The quantity.
+    """
+
+    name: str
+    amount: int
+    quantity: int
+
+    @property
+    def dict(self) -> dict:
+        """Converts a LineItem to a dictionary"""
+        return {
+            "name": self.name,
+            "amount": self.amount,
+            "quantity": self.quantity,
+        }
+
+    @classmethod
+    def from_dict(cls, value: dict) -> "LineItem":
+        """Creates a LineItem from a dictionary.
+
+        The dictionary has to have the following keys: `name`, `amount`, `quantity`
+        """
+        return cls(
+            name=value["authorization"],
+            amount=value["amount"],
+            quantity=value["quantity"],
+        )
+
+    @classmethod
+    def from_dict_many(cls, values: list[dict]) -> list["LineItem"]:
+        """Creates a list of LineItem from a list of dictionaries.
+
+        The dictionary has to have the following keys: `name`, `amount`, `quantity`
+        """
+        return [
+            cls(
+                name=item["name"],
+                amount=item["amount"],
+                quantity=item["quantity"],
+            )
+            for item in values
+        ]
+
+
+@dataclass
+class Tax:
+    """A dataclass for Tax.
+
+    Attributes:
+        name: The name of the tax.
+        amount: The price of the tax.
+    """
+
+    name: str
+    amount: int
+
+    @property
+    def dict(self) -> dict:
+        """Converts a Tax to a dictionary"""
+        return {
+            "name": self.name,
+            "amount": self.amount,
+        }
+
+    @classmethod
+    def from_dict(cls, value: dict) -> "Tax":
+        """Creates a Tax from a dictionary.
+
+        The dictionary has to have the following keys: `name`, `amount`
+        """
+        return cls(
+            name=value["name"],
+            amount=value["amount"],
+        )
+
+    @classmethod
+    def from_dict_many(cls, values: list[dict]) -> list["Tax"]:
+        """Creates a list of Tax from a list of dictionaries.
+
+        The dictionary has to have the following keys: `name`, `amount`
+        """
+        return [
+            cls(
+                name=item["name"],
+                amount=item["amount"],
+            )
+            for item in values
+        ]
+
+
+@dataclass
+class SplitAccount:
+    """A dataclass for SplitAccount.
+
+    Attributes:
+        subaccount: The id of the sub account.
+        share: The share of the split account the sub account should have.
+    """
+
+    subaccount: str
+    share: Union[int, float]
+
+    @property
+    def dict(self) -> dict:
+        """Converts a SplitAccount to a dictionary"""
+        return {
+            "subaccount": self.subaccount,
+            "share": self.share,
+        }
+
+    @classmethod
+    def from_dict(cls, value: dict) -> "SplitAccount":
+        """Creates a SplitAccount from a dictionary.
+
+        The dictionary has to have the following keys: `subaccount`, `share`
+        """
+        return cls(
+            subaccount=value["subaccount"],
+            share=value["share"],
+        )
+
+    @classmethod
+    def from_dict_many(cls, values: list[dict]) -> list["SplitAccount"]:
+        """Creates a list of SplitAccount from a list of dictionaries.
+
+        The dictionary has to have the following keys: `subaccount`, `share`
+        """
+        return [
+            cls(
+                subaccount=item["subaccount"],
+                share=item["share"],
+            )
+            for item in values
+        ]
+
+
+@dataclass
+class Recipient:
+    """A dataclass for Recipient.
+
+    Attributes:
+        type: The type of recipient e.g., RecipientType.NUBAN, RecipientType.BASA.
+        name: The name.
+        bank_code: The bank code.
+        account_number: The account number of recipient.
+    """
+
+    type: "RecipientType"
+    name: str
+    bank_code: str
+    account_number: str
+
+    @property
+    def dict(self) -> dict:
+        """Converts a Recipient to a dictionary"""
+        return {
+            "type": self.type,
+            "name": self.name,
+            "bank_code": self.bank_code,
+            "account_number": self.account_number,
+        }
+
+    @classmethod
+    def from_dict(cls, value: dict) -> "Recipient":
+        """Creates a Recipient from a dictionary.
+
+        The dictionary has to have the following keys: `type`, `name`, `bank_code`, `account_number`
+        """
+        return cls(
+            type=value["type"],
+            name=value["name"],
+            bank_code=value["bank_code"],
+            account_number=value["account_number"],
+        )
+
+    @classmethod
+    def from_dict_many(cls, values: list[dict]) -> list["Recipient"]:
+        """Creates a list of SplitAccount from a list of dictionaries.
+
+        The dictionary has to have the following keys: `type`, `name`, `bank_code`, `account_number`
+        """
+        return [
+            cls(
+                type=item["type"],
+                name=item["name"],
+                bank_code=item["bank_code"],
+                account_number=item["account_number"],
+            )
+            for item in values
+        ]
+
+
+@dataclass
+class TransferInstruction:
+    """A dataclass for TransferInstruction.
+
+    Attributes:
+        amount: The amount to be transferred.
+        recipient: The beneficiary of the transaction.
+        reference: The reference for the transaction.
+        reason: The narration of the transaction.
+    """
+
+    amount: int
+    recipient: str
+    reference: Optional[str]
+    reason: Optional[str]
+
+    @property
+    def dict(self) -> dict:
+        """Converts a TransferInstruction to a dictionary"""
+        return {
+            "amount": self.amount,
+            "reference": self.reference,
+            "recipient": self.recipient,
+            "reason": self.reason,
+        }
+
+    @classmethod
+    def from_dict(cls, value: dict) -> "TransferInstruction":
+        """Creates a TransferInstruction from a dictionary.
+
+        The dictionary has to have the following keys: `amount`, `reference`, `recipient`, `reason`
+        """
+        return cls(
+            amount=value["amount"],
+            reference=value.get("reference"),
+            recipient=value["recipient"],
+            reason=value.get("reason"),
+        )
+
+    @classmethod
+    def from_dict_many(cls, values: list[dict]) -> list["TransferInstruction"]:
+        """Creates a list of TransferInstruction from a list of dictionaries.
+
+        The dictionary has to have the following keys: `amount`, `reference`, `recipient`, `reason`
+        """
+        return [
+            cls(
+                amount=item["amount"],
+                reference=item.get("reference"),
+                recipient=item["recipient"],
+                reason=item.get("reason"),
+            )
+            for item in values
+        ]
+
+
+class Response(NamedTuple):
+    """
+    A namedtuple containing the data gotten from making a request to paystack's API endpoints.
+
+    All wrapper methods returns an instance of `Response` which can be used as a tuple following
+    this format `(status_code, status, message, data)`. Accessing by attributes is also possible
+    say for example `response` is an instance of `Response`, we can access the data in the
+    response like so `response.data`
+
+    Attributes:
+        status_code: The response status code
+        status: A flag for the response status
+        message: Paystack response message
+        data: Data sent from paystack's server if any.
+    """
+
+    status_code: int
+    status: bool
+    message: str
+    data: Optional[Union[dict[str, Any], list[dict[str, Any]]]]
