fdb63b3da47e693ae74450644f6e7b798d4a0507
Gbenga Adeyi <adeyigbenga005@gmail.com>
2025-12-07 23:57:50 +0100

    Bump package version

--- src/pypaystack2/_metadata.py
+++ src/pypaystack2/_metadata.py
@@ -1,5 +1,5 @@
 __title__ = "pypaystack2"
-__version__ = "3.0.2"
+__version__ = "3.1.0"
 __author__ = [{"name": "Gbenga Adeyi", "email": "adeyigbenga005@gmail.com"}]
 __license__ = "MIT"
 __copyright__ = "Copyright 2025."
