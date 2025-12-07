0c9c2845c2a8dbefa93eed8ff92234fb37f1cb62
Gbenga Adeyi <adeyigbenga005@gmail.com>
2025-12-07 13:25:20 +0100

    Add some webhook model

---
+++ src/pypaystack2/webhook/models.py
@@ -0,0 +1,41 @@
+from enum import StrEnum
+from typing import Any
+
+from pydantic import BaseModel
+
+
+class EndpointAddress(BaseModel):
+    host: str | None = None
+    port: int | None = None
+
+
+class PaystackWebhookEvent(StrEnum):
+    CHARGE_DISPUTE_CREATE = "charge.dispute.create"
+    CHARGE_DISPUTE_REMIND = "charge.dispute.remind"
+    CHARGE_DISPUTE_RESOLVE = "charge.dispute.resolve"
+    CHARGE_SUCCESS = "charge.success"
+    CUSTOMER_IDENTIFICATION_FAILED = "customeridentification.failed"
+    CUSTOMER_IDENTIFICATION_SUCCESS = "customeridentification.success"
+    DEDICATED_ACCOUNT_ASSIGN_FAILED = "dedicatedaccount.assign.failed"
+    DEDICATED_ACCOUNT_ASSIGN_SUCCESS = "dedicatedaccount.assign.success"
+    INVOICE_CREATE = "invoice.create"
+    INVOICE_PAYMENT_FAILED = "invoice.payment_failed"
+    INVOICE_UPDATE = "invoice.update"
+    PAYMENT_REQUEST_PENDING = "paymentrequest.pending"
+    PAYMENT_REQUEST_SUCCESS = "paymentrequet.success"
+    REFUND_FAILED = "refund.failed"
+    REFUND_PENDING = "refund.pending"
+    REFUND_PROCESSED = "refund.processed"
+    REFUND_PROCESSING = "refund.processing"
+    SUBSCRIPTION_CREATE = "subscription.create"
+    SUBSCRIPTION_DISABLE = "subscription.disable"
+    SUBSCRIPTION_EXPIRING_CARDS = "subscription.expiring_cards"
+    SUBSCRIPTION_NOT_RENEW = "subscription.not_renew"
+    TRANSFER_FAILED = "transfer.failed"
+    TRANSFER_SUCCESS = "transfer.success"
+    TRANSFER_REVERSED = "transfer.reversed"
+
+
+class PaystackWebhookPayload(BaseModel):
+    event: PaystackWebhookEvent
+    data: dict[str, Any]
