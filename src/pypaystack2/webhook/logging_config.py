cb4c5ff6d79711d6aa57b780b290532e3a08854f
Gbenga Adeyi <adeyigbenga005@gmail.com>
2025-12-07 13:21:50 +0100

    Add logging config

---
+++ src/pypaystack2/webhook/logging_config.py
@@ -0,0 +1,51 @@
+from typing import Any
+
+SERVER_LOGGING_NAME = "PyPaystack2 Webhook Proxy Server"
+WEBHOOK_CLI_LOGGING_NAME = "PyPaystack2 Webhook CLI"
+LOGGING_CONFIG: dict[str, Any] = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "default": {
+            "()": "uvicorn.logging.DefaultFormatter",
+            "fmt": "%(levelprefix)s %(message)s",
+            "use_colors": True,
+        },
+        "access": {
+            "()": "uvicorn.logging.AccessFormatter",
+            "fmt": '%(levelprefix)s %(client_addr)s - "%(request_line)s" %(status_code)s',  # noqa: E501
+        },
+    },
+    "handlers": {
+        "default": {
+            "formatter": "default",
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stderr",
+        },
+        "access": {
+            "formatter": "access",
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stdout",
+        },
+        "console": {
+            "formatter": "default",
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stdout",
+        },
+    },
+    "loggers": {
+        "uvicorn": {"handlers": ["default"], "level": "INFO", "propagate": False},
+        "uvicorn.error": {"level": "INFO"},
+        "uvicorn.access": {"handlers": ["access"], "level": "INFO", "propagate": False},
+        SERVER_LOGGING_NAME: {
+            "handlers": ["console"],
+            "level": "INFO",
+            "propagate": False,
+        },
+        WEBHOOK_CLI_LOGGING_NAME: {
+            "handlers": ["console"],
+            "level": "INFO",
+            "propagate": False,
+        },
+    },
+}
